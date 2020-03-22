b01eff1e811550ec4dc45fa3a15efe2f6f61c76e
raparicio6 <rodrigoaparicio6@gmail.com>
2020-03-22 13:10:10 -0300

    adding dotenv package in config

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
+const ENVIRONMENT = process.env.NODE_ENV || 'development';
+const dotenv = require('dotenv');
+
+if (ENVIRONMENT !== 'production') dotenv.config();
+
 const config = {
   common: {
     api: {
       bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
       parameterLimit: process.env.API_PARAMETER_LIMIT,
       port: process.env.PORT
-    },
-    headers: {
-      apiDate: process.env.API_DATE || 'X-API-Date',
-      packageVersion: process.env.PACKAGE_VERSION || 'X-Package-Version',
-      nodeVersion: process.env.NODE_VERSION || 'X-Node-Version'
     }
   }
 };
