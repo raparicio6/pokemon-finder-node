11eb6fd9a79571ccb33a036c88e85dbd16119af7
raparicio6 <rodrigoaparicio6@gmail.com>
2020-04-25 21:32:57 -0300

    removing unneccesary braces

--- app/controllers/pokemons.js
+++ app/controllers/pokemons.js
@@ -1,11 +1,10 @@
 const { getPokemon, getAllPokemons } = require('../services/pokemons');
 const { formatPokemons, getHashedPokemonsNames } = require('../serializers/pokemons');
 
-exports.getPokemons = (req, res, next) => {
+exports.getPokemons = (req, res, next) =>
   Promise.all(req.query.pokemonsNames.map(getPokemon))
     .then(pokemons => res.send(formatPokemons(pokemons)))
     .catch(next);
-};
 
 exports.getAllPokemonsHashedNames = (req, res, next) =>
   getAllPokemons()
