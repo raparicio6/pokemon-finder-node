aef8481eaa63bdb0bb17491e43fc3637de4c6a9c
raparicio6 <rodrigoaparicio6@gmail.com>
2020-05-15 22:33:12 -0300

    using reduce instead of for each

--- app/serializers/pokemons.js
+++ app/serializers/pokemons.js
@@ -9,25 +9,21 @@ exports.serializePokemons = pokemons => ({
   }))
 });
 
-exports.getHashedPokemonsNames = results => {
-  const hashedPokemonsNames = {};
-  results
-    .map(pokemon => pokemon.name)
-    .forEach(pokemonName => {
-      const firstCharacter = pokemonName[0];
-      const secondCharacter = pokemonName[1];
+exports.getHashedPokemonsNames = results =>
+  results.reduce((hashedPokemonsNames, { name }) => {
+    const firstCharacter = name[0];
+    const secondCharacter = name[1];
 
-      if (!(firstCharacter in hashedPokemonsNames)) {
-        hashedPokemonsNames[firstCharacter] = {};
-      }
-      if (!(secondCharacter in hashedPokemonsNames[firstCharacter])) {
-        hashedPokemonsNames[firstCharacter] = {
-          ...hashedPokemonsNames[firstCharacter],
-          [secondCharacter]: []
-        };
-      }
+    if (!(firstCharacter in hashedPokemonsNames)) {
+      hashedPokemonsNames[firstCharacter] = {};
+    }
+    if (!(secondCharacter in hashedPokemonsNames[firstCharacter])) {
+      hashedPokemonsNames[firstCharacter] = {
+        ...hashedPokemonsNames[firstCharacter],
+        [secondCharacter]: []
+      };
+    }
 
-      hashedPokemonsNames[firstCharacter][secondCharacter].push(pokemonName);
-    });
-  return hashedPokemonsNames;
-};
+    hashedPokemonsNames[firstCharacter][secondCharacter].push(name);
+    return hashedPokemonsNames;
+  }, {});
